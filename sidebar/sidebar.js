013ce1bbd0149ae114cdb09d678289c04ebd5e84
Aluex <aluex.cpp@gmail.com>
2013-07-15 00:29:14 +0800

    Sidebar update

--- sidebar/sidebar.js
+++ sidebar/sidebar.js
@@ -1,3 +1,57 @@
+var ColorGroup = [[255,200,200],[200,255,200],[200,200,255]],
+    highlightColor = [255,255,255], lastFocus=[0,0], defaultColor=[242,242,242], currentWeek= 0;
+var totalNum = [6, 7, 8];
+function makeColor(colorList)
+{
+    return 'rgb('+String(colorList[0])+','+String(colorList[1])+','+String(colorList[2])+')';
+}
+var randomColor = function(){
+    return makeColor(ColorGroup[Math.floor((Math.random()*ColorGroup.length))]);
+};
+function setFocus(week, sub)
+{
+    $('#sections ul li:eq('+String(lastFocus[0])+')').css('background-color', makeColor(defaultColor));
+    $('#subsections ul li:eq('+String(lastFocus[1])+')').css('background-color', makeColor(defaultColor));
+
+    lastFocus = [week, sub];
+    thisColor = $('#subsections .colorTag:eq('+String(sub)+')').css('background-color');
+    $('#sections ul li:eq('+String(week)+')').css('background-color', makeColor(highlightColor));
+    $('#subsections ul li:eq('+String(sub)+')').css('background-color', thisColor);
+    $('#units ul li').css('background-color', thisColor);
+}
+function markDone(num)
+{
+    $('#units ul li:eq('+String(num)+')').css('opacity','0.7');
+}
+function clearDone(num)
+{
+    $('#units ul li:eq('+String(num)+')').css('opacity','1.0');
+}
+function markDoneTill(num)
+{
+    $('#units ul li:lt('+String(num)+')').css('opacity','0.7');
+}
+function clearDone(num)
+{
+    $('#units ul li:lt('+String(num)+')').css('opacity','1.0');
+}
+function clearDoneAll()
+{
+    clearDone(totalNum[2]-1);
+}
 $(document).ready(function(){
-	$()
+	$('#subsections .colorTag').each(function(index, element){
+        $(element).css('background-color',randomColor());
+    });
+    $('#subsections ul li').click(function(e){
+        setFocus(currentWeek, $(this).index());
+        clearDoneAll();
+    });
+    $('#sections ul li').click(function(e){
+        setFocus($(this).index(),0);
+        currentWeek = $(this).index();
+    });
+    setFocus(1,1);
+    markDone(3);
 });
+
