3cfc6d651fcb2a2375e6a75284e650bdfcacadf6
Aluex <aluex.cpp@gmail.com>
2013-07-16 01:51:08 +0800

    update

--- build_base/src/unitPage/unit.js
+++ build_base/src/unitPage/unit.js
@@ -1,4 +1,4 @@
-var verticalControl = {};
+var verticalControl = {}, p, coursePath = "../../../data/2013_Spring_Tsinghua";
 $(document).ready(function(){
     verticalControl.current = 0;
         verticalControl.start = 0;
@@ -9,12 +9,29 @@ $(document).ready(function(){
         verticalControl.controler = $('#nav_scroll');*/
         verticalControl.num = $('.horizontalButton').length;
         var refresh = function() {
+            if(verticalControl.current==0)$('#left_button').addClass('ui-disabled');
+            else if(verticalControl.current==verticalControl.num-1)$('#right_button').addClass('ui-disabled');
+            else {
+                $('#left_button').removeClass('ui-disabled');
+                $('#right_button').removeClass('ui-disabled');
+            }
             $('.horizontalButton').removeClass('ui-btn-active');
             $('.horizontalButton:eq('+String(verticalControl.current)+')').addClass('ui-btn-active');
             $('#nav_scroll').css('left',String(-verticalControl.start * $('.horizontalButton').width())+'px');
+            ///////////////////////////////////
+            var Res = '', verticaltmp = p.verticals[verticalControl.current];
+            for(var i in verticaltmp.components)
+            {
+                Res += verticaltmp.components[i].url_name;
+            }
+            $('#content_holder').html('');
+            ///////////////////////////////////
         };
         var move = function (k) {
-            if(k<0||k>=verticalControl.num)return;
+            if(k<0||k>=verticalControl.num)
+            {
+                return;
+            }
             verticalControl.current = k;
             if(k - verticalControl.start +1 > verticalControl.showNum){
                 verticalControl.start = k - verticalControl.showNum + 1;
@@ -44,9 +61,28 @@ $(document).ready(function(){
     });
     $('#left_button').click(function(){verticalControl.turnLeft();});
     $('#right_button').click(function(){verticalControl.turnRight();});
-    p = $.parseJSON(localStorage.getItem('unitData'));
+    //p = $.parseJSON(localStorage.getItem('unitData'));
+    p = obj[0].sequentials[0];
     if(p!=null)
     {
-        
+        if(p.display_name)
+            $('#title').html(document.title = p.display_name);
+        else
+            $('#title').html(document.title = p.url_name);
+        for(var i in p.verticals)
+        {
+            var tmp = p.verticals[i];
+            if(tmp.components==null)continue;
+            if(tmp.components[0].type=="problem")
+                $('#nav_scroll_container').append(
+                    '<a href="#" data-theme="c" data-role="button" data-iconpos="left" data-inline="true" data-icon="grid" class="horizontalButton ui-btn-active">'+tmp.display_name+'</a>'
+                );
+            else
+                $('#nav_scroll_container').append(
+                    '<a href="#" data-theme="c" data-role="button" data-iconpos="left" data-inline="true" data-icon="star" class="horizontalButton ui-btn-active">'+tmp.display_name+'</a>'
+                );
+        }
+
     }
+    verticalControl.refresh();
 });
---
+++ build_base/src/unitPage/unit_loader.js
@@ -0,0 +1,105 @@
+var initSidebar = function () {
+
+    var getHierarchy = function (coursepath) {
+        var xmlDom = loadXmlFile(coursepath + "course.xml");
+        xmlDom = xmlDom.getElementsByTagName("course")[0];
+        var url_name = xmlDom.getAttribute("url_name");
+        xmlDom = loadXmlFile(coursepath + "course/" + url_name + ".xml");
+        xmlDom = xmlDom.getElementsByTagName("course")[0];
+        var chapterSet = xmlDom.getElementsByTagName("chapter");
+        var hierarchy = [];
+        for (var chapterIndex = 0; chapterIndex < chapterSet.length; chapterIndex++) {
+            var chapter = chapterSet[chapterIndex];
+            url_name = chapter.getAttribute("url_name");
+            hierarchy.push(loadChapter(coursepath, url_name));
+        }
+        return hierarchy;
+    }
+    var loadChapter = function (coursepath, url_name) { //Week or Section
+        var xmlDom = loadXmlFile(coursepath + "chapter/" + url_name + ".xml");
+        xmlDom = xmlDom.getElementsByTagName("chapter")[0];
+        var chapter = {};
+        chapter["url_name"] = url_name;
+        /* possible attributes:
+         * display_name
+         * graceperiod
+         * start
+         * xqa_key
+         */
+        for (var attrIdx = 0; attrIdx < xmlDom.attributes.length; attrIdx++) {
+            var attribute = xmlDom.attributes.item(attrIdx);
+            chapter[attribute.nodeName] = attribute.nodeValue;
+        }
+        chapter["sequentials"] = [];
+        var sequentialSet = xmlDom.getElementsByTagName("sequential");
+        for (var sequentialIndex = 0; sequentialIndex < sequentialSet.length; sequentialIndex++) {
+            var sequential = sequentialSet[sequentialIndex];
+            url_name = sequential.getAttribute("url_name");
+            chapter["sequentials"].push(loadSequential(coursepath, url_name));
+        }
+        return chapter;
+    }
+    var loadSequential = function (coursepath, url_name) { //Subsection
+        var xmlDom = loadXmlFile(coursepath + "sequential/" + url_name + ".xml");
+        xmlDom = xmlDom.getElementsByTagName("sequential")[0];
+        var sequential = {};
+        sequential["url_name"] = url_name;
+        /* possible attributes:
+         * display_name
+         * graceperiod
+         * start
+         * xqa_key
+         * due
+         * format
+         * graded
+         * rerandomize
+         * showanswer
+         */
+        for (var attrIdx = 0; attrIdx < xmlDom.attributes.length; attrIdx++) {
+            var attribute = xmlDom.attributes.item(attrIdx);
+            sequential[attribute.nodeName] = attribute.nodeValue;
+        }
+        sequential["verticals"] = [];
+        var verticalSet = xmlDom.getElementsByTagName("vertical");
+        for (var verticalIndex = 0; verticalIndex < verticalSet.length; verticalIndex++) {
+            var vertical = verticalSet[verticalIndex];
+            url_name = vertical.getAttribute("url_name");
+            sequential["verticals"].push(loadVertical(coursepath, url_name));
+        }
+        return sequential;
+    }
+    var loadVertical = function (coursepath, url_name) { //Unit
+        var xmlDom = loadXmlFile(coursepath + "vertical/" + url_name + ".xml");
+        xmlDom = xmlDom.getElementsByTagName("vertical")[0];
+        var vertical = {};
+        vertical["url_name"] = url_name;
+        /* possible attributes:
+         * attempts
+         * display_name
+         * start
+         * due
+         * format
+         * graded
+         * rerandomize
+         * showanswer
+         */
+        for (var attrIdx = 0; attrIdx < xmlDom.attributes.length; attrIdx++) {
+            var attribute = xmlDom.attributes.item(attrIdx);
+            vertical[attribute.nodeName] = attribute.nodeValue;
+        }
+        vertical["components"] = [];
+        for (var childIdx = 0; childIdx < xmlDom.childNodes.length; childIdx++) {
+            var child = xmlDom.childNodes.item(childIdx);
+            if (child.nodeName != "#text") {
+                vertical["components"].push({
+                    "type": child.nodeName,
+                    "url_name": child.getAttribute("url_name")
+                });
+            }
+        }
+        return vertical;
+    }
+    return getHierarchy('../../../data/2013_Spring_Tsinghua/');
+}
+var obj = initSidebar();
+//alert(obj);
