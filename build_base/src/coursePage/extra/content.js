04c88e27f206bbb375aed95fff0b676b7d16ab6f
cgcgbcbc <cgcgbcbc@163.com>
2013-07-20 23:34:07 +0800

    modify coursepage content

--- build_base/src/coursePage/extra/content.js
+++ build_base/src/coursePage/extra/content.js
@@ -2,7 +2,7 @@ var initContent = function(bodyInner){
 	var contentIframe = bodyInner.find('#main');
 	var storage = window.localStorage;
 	var coursepath = (storage.hasOwnProperty("raw_coursepath"))?storage.getItem("raw_coursepath"):"../data/2013_Spring_Tsinghua/";
-	storage.setItem("coursepath","../../"+coursepath);
+	storage.setItem("coursepath",coursepath);
 	$.get(coursepath+"info/updates.html",function(data){
 		contentIframe.html(data);
 	});
